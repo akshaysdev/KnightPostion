c9a4a36027131d12fecb2d409739820765982700
Akshay S <52912821+Akshayus27@users.noreply.github.com>
2022-08-11 19:01:14 +0000

    Initial Commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { possibleKnightPositions } = require('./board');
 const { extractInput } = require('./helper');
+const { columnLabel } = require('./constants')
 
 const ReadLine = require('readline');
 const readLine = ReadLine.createInterface({
@@ -9,8 +10,10 @@ const readLine = ReadLine.createInterface({
 
 readLine.on('line', function(line) {
   const { row, column } = extractInput(line);
-  if (!Number.isNaN(+row)) {
-    console.error('Enter correct position, E.g., a1 or A1');
+  if (!Number.isNaN(+row) || column < 1 || column > 8 || !columnLabel[row]) {
+    console.error('ENTER CORRECT POSITION);
+    console.error('E.g., a1 or A1, In the range A to H , 1 to 8');
+    return;
   }
   const result = possibleKnightPositions({ row, column });
   console.log(result.join(', '));
