95ecef4040ce08788ae695fc54cf32218f2f1aad
Akshay S <52912821+Akshayus27@users.noreply.github.com>
2022-08-11 19:02:07 +0000

    position check by row and column

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const readLine = ReadLine.createInterface({
 readLine.on('line', function(line) {
   const { row, column } = extractInput(line);
   if (!Number.isNaN(+row) || column < 1 || column > 8 || !columnLabel[row]) {
-    console.error('ENTER CORRECT POSITION);
+    console.error('ENTER CORRECT POSITION');
     console.error('E.g., a1 or A1, In the range A to H , 1 to 8');
     return;
   }
